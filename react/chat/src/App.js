db16fe789575a32b6d13ae74631b784e070711d1
nipan09 <pandey.nishant489@gmail.com>
2021-05-22 22:59:45 +0530

    connection made with django

--- react/chat/src/App.js
+++ react/chat/src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import { w3cwebsocket as W3CWebSocket } from "websocket";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import TextField from '@material-ui/core/TextField';
+import Link from '@material-ui/core/Link';
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+import Container from '@material-ui/core/Container';
+import Card from '@material-ui/core/Card';
+import CardHeader from '@material-ui/core/CardHeader';
+import Paper from '@material-ui/core/Paper';
+import Avatar from '@material-ui/core/Avatar';
+
+import { withStyles } from "@material-ui/core/styles";
+
+class App extends Component{
+
+  state = {
+    isLoggedIn: false,
+    messages: [],
+    value: '',
+    name: '',
+    room: 'test',
+  }
+
+  client = new W3CWebSocket('ws://127.0.0.1:8000/ws/chat/' + this.state.room + '/');  
+  
+  componentDidMount(){
+    this.client.onopen = () => { 
+      console.log('WebSocket Client Connected');
+    }
+  }
+
+  render(){
+    return(
+      <Container component="main" maxWidth="xs">
+        {this.state.isLoggedIn ?  :}
+      </Container>
+    )
+  }
 }
 
-export default App;
+export default(App)
