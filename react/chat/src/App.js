1f81b63aad7c751155485e47707ed14e38099bbc
nipan09 <pandey.nishant489@gmail.com>
2021-05-22 23:03:41 +0530

    style added

--- react/chat/src/App.js
+++ react/chat/src/App.js
@@ -15,6 +15,30 @@ import Avatar from '@material-ui/core/Avatar';
 
 import { withStyles } from "@material-ui/core/styles";
 
+const useStyles = theme => ({
+  paper: {
+    marginTop: theme.spacing(8),
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main,
+  },
+  form: {
+    width: '100%', // Fix IE 11 issue.
+    marginTop: theme.spacing(1),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+  root: {
+    boxShadow: 'none',
+  }
+});
+
+
 class App extends Component{
 
   state = {
@@ -42,4 +66,4 @@ class App extends Component{
   }
 }
 
-export default(App)
+export default withStyles(useStyles)(App)
